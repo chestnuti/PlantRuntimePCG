b10a235226e6c935d34eaf787859eafc23baadb7
chestnuti <chestnut2612@outlook.com>
2026-07-09 19:51:15 +0800

    Create project

---
+++ Source/PlantRuntimePCG/PlantRuntimePCG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PlantRuntimePCG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PlantRuntimePCG, "PlantRuntimePCG" );
